8fb164dc153c45127628bd8167b0f5b487f689ad
Symchowicz <symchowiczbenji@gmail.com>
2021-04-18 12:34:47 +0200

    changement background

--- entrainement pygame Benjamin/Game.py
+++ entrainement pygame Benjamin/Game.py
@@ -9,20 +9,20 @@ pygame.display.set_caption("Trouver un nom de jeu ")
 # definir la taille de la fenetre :
 fenetre = pygame.display.set_mode((1440, 1024))
 
-Joueur = joueur()
+joueur = joueur()
 
 running = True
 
 # Choisir un background ( le background ici présent n'était qu'un test et n'est pas définitif )
-background = pygame.image.load('assets/boxe.jpg')
+background = pygame.image.load('assets/arène.jpg')
 
 while running:
     #chargement de background
-    fenetre.blit(background, (-800, 50))
+    fenetre.blit(background, (-300,0))
 
     #test chargement joueur
 
-    fenetre.blit(Joueur.image, Joueur.rect)
+    fenetre.blit(joueur.image, joueur.rect)
 
     #permet de mettre a jour l'écran :
     pygame.display.flip()
--- entrainement pygame Benjamin/classjoueur.py
+++ entrainement pygame Benjamin/classjoueur.py
@@ -7,7 +7,7 @@ class joueur(pygame.sprite.Sprite):
         super().__init__()
         self.pv = 100
         self.max_health = 100
-        self.image = pygame.image.load('assets/akuma détouré.png')
+        self.image = pygame.image.load('assets/akuma.png')
         self.rect = self.image.get_rect()
         self.rect.x = 1000
         self.rect.y = 350
