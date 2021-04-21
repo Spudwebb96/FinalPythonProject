066155df08a5058d1f8a69fe611c41c6df80b65b
Symchowicz <symchowiczbenji@gmail.com>
2021-04-21 18:31:51 +0200

    entrainement : ajout d un button, mise a jour des classes

--- entrainement pygame Benjamin/Game.py
+++ entrainement pygame Benjamin/Game.py
@@ -4,7 +4,7 @@ from classjoueur import joueur
 
 
 # donner le nom a la fenetre de jeu :
-pygame.display.set_caption("Trouver un nom de jeu ")
+pygame.display.set_caption("Words Legends")
 
 # definir la taille de la fenetre :
 fenetre = pygame.display.set_mode((1440, 1024))
@@ -13,24 +13,55 @@ joueur = joueur()
 
 running = True
 
-# Choisir un background ( le background ici présent n'était qu'un test et n'est pas définitif )
-background = pygame.image.load('assets/arène.jpg')
+# ## LE MENU
 
-# essaie d'ajout musique combat :
-pygame.mixer.music.load('assets/battle-music.mp3')
-pygame.mixer.music.play(-1)
+# le background
+backgroundmenu = pygame.image.load('assets/ecran menu.jpg')
 
-while running:
-    # chargement de background
-    fenetre.blit(background, (-300, 0))
+# le titre
+Title_game = pygame.image.load('assets/titre.png')
+Title_game = pygame.transform.scale(Title_game, (1500, 1100))
 
-    # test chargement joueur
-    fenetre.blit(joueur.image, joueur.rect)
+# le button
+Button_play = pygame.image.load('assets/button play.png')
+Button_play = pygame.transform.scale(Button_play, (200, 200))
+Button_play_rect = Button_play.get_rect()
+Button_play_rect.x = 633
+Button_play_rect.y = 500
+# les backgrounds
+
+background1 = pygame.image.load('assets/arène.jpg')
+
+
+while joueur.is_playing:
+
+    if joueur.in_menu:
+        joueur.update(fenetre)
+
+        fenetre.blit(backgroundmenu, (0, 0))
+
+        fenetre.blit(Title_game, (-50, -50))
+
+        fenetre.blit(Button_play, (633, 500))
+
+
+    else:
+        # chargement de background
+        fenetre.blit(background1, (-300, 0))
+
+        # test chargement joueur
+        fenetre.blit(joueur.image, joueur.rect)
+
 
     # permet de mettre a jour l'écran :
     pygame.display.flip()
 
     for event in pygame.event.get():
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if Button_play_rect.collidepoint(event.pos):
+                joueur.in_menu = False
+
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
--- entrainement pygame Benjamin/classjoueur.py
+++ entrainement pygame Benjamin/classjoueur.py
@@ -6,10 +6,13 @@ pygame.init()
 class joueur(pygame.sprite.Sprite):
 
     def __init__(self):
-        super().__init__()
+        self.is_playing = True
+        self.in_menu = True
         self.pv = 100
         self.max_health = 100
         self.image = pygame.image.load('assets/akuma.png')
         self.rect = self.image.get_rect()
         self.rect.x = 1000
         self.rect.y = 350
+
+
