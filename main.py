e307266b925fed64f694b2bb2d073405137bae9d
Jojo <machado.johanna@icloud.com>
2021-04-23 15:20:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
 pygame.init()
 
+frame_per_sec = pygame.time.Clock()
+
 display_surface = pygame.display.set_mode((1440,1024))
 
 pygame.display.set_caption("Words Legends")
 
-backgroundmenu = pygame.image.load()
+backgroundmenu = pygame.image.load('assets/fond_menu.jpg')
+
+x = True
+
+while x:
+
+    pygame.display.update()
+
+    display_surface.blit(backgroundmenu,(0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    frame_per_sec.tick(60)
